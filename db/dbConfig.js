8b2b41507cdbce60ceeeeba91c3e156284fa1859
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-22 14:35:15 -0400

    PG-Promise setup completed

---
+++ db/dbConfig.js
@@ -0,0 +1,30 @@
+//PG-Promise Setup
+const pgp = require("pg-promise")();
+require("dotenv").config();
+
+
+//Create the connection
+const cn = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER
+}
+
+//Open the connection
+const db = pgp(cn);
+
+//If we want more information about the connection
+db.connect()
+  .then((cn) => {
+    const { user, host, port, database } = cn.client;
+    console.log(
+      "\x1b[90m" +
+        `Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
+        "\x1b[0m"
+    );
+    cn.done();
+  })
+  .catch((error) => console.log("database connection error", error));
+
+module.exports = db;
