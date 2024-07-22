1efa19728ed3bd9afaa3a98c15251450dfc53844
incognito1025 <nkhanamart@gmail.com>
2024-07-22 17:13:47 -0400

    added function for Index stats.get(/) route in controllers/statsController.js

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const stats = express.Router();
+const { getAllStats } = require("../queries/stats.js");
+const { db } = require( "../db/dbConfig.js");
 
 //INDEX
-stats.get("/", (req, res) => {
-    res.json({ status: "ok" });
+stats.get("/", async (req, res) => {
+    const allStats = await getAllStats();
+    if (allStats[0]) {
+    res.status(200).json(allStats);
+    } else {
+    res.status(500).json({error:"server error"});
+    }
 });
+
 
 
 
