91e0d338a13c0f268be0e17644bd02efce2b7132
incognito1025 <nkhanamart@gmail.com>
2024-07-22 19:25:00 -0400

    added CREATE function for queiry in stats.js

--- queries/stats.js
+++ queries/stats.js
@@ -11,6 +11,7 @@ const getAllStats = async () => {
     }
   };
 
+
 const getStat = async (id) => {
   try {
       const stat = await db.one("SELECT * FROM stats WHERE id=$1", id);
@@ -20,7 +21,20 @@ const getStat = async (id) => {
   }
 };
 
+//CREATE
+const createStat = async (stat) => {
+  try {
+    const newStat = await db.one(
+      "INSERT INTO stats (name, position, mins_played, assists, goals, perc_score, rating) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [stat.name, stat.position, stat.mins_played, stat.assists, stat.goals, stat.perc_score, stat.rating] 
+    );
+    return newStat;
+  } catch (error) {
+    throw error;
+  }
+};
+
 
 //.any() function takes string as SQL statement as first argument. Will accept any return from the database, no rows, one row. etc.
 
-module.exports = { getAllStats, getStat };
+module.exports = { getAllStats, getStat, createStat };
