6f5e0c94ea35751ad58dca854bc6da9fa0454630
incognito1025 <nkhanamart@gmail.com>
2024-07-22 13:48:47 -0400

    added db/schema.sql file w/ code to create database and table

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+-- db/schemal.sql
+DROP DATABASE IF EXISTS stats_dev;
+CREATE DATABASE stats_dev;
+
+\c stats_dev;
+
+CREATE TABLE stats (
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+position TEXT,
+mins_played INTEGER,
+assists INTEGER,
+goals INTEGER,
+perc_score NUMERIC(5, 2),
+rating NUMERIC(5, 2)
+);
+
