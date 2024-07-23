ddbefffe53917e9db48932d81d2b869933e45316
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-22 21:45:19 -0400

    Added the checkName validity in checkStats.js and imported it in statsController.js

---
+++ validations/checkStats.js
@@ -0,0 +1,39 @@
+const checkName = async (req, res, next) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({error: "Name is required"});
+    }
+
+    if (typeof name !== "string") {
+        return res.status(400).json({error: "Name must be a string"});
+    }
+
+    const isValidName = (str) => {
+        for (let char of str) {
+            if (
+                !(char >= "A" && char <= "Z") && 
+        !(char >= "a" && char <= "z") && !(char === " ")) {
+            return false;
+        }     
+        }
+        return true;
+    }
+
+    if (isValidName(name)) {
+        return next();
+    } else {
+        res.status(400).json({error: "Name is invalid"})
+    }
+}
+
+
+const checkRating = async (req, res, next) => {
+    if (req.body.rating) {
+        return next();
+    } else {
+        res.status(400).json({error : "Rating is required"})
+    }
+}
+
+module.exports = {checkRating, checkName};
