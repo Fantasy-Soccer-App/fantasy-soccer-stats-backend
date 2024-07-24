7407cd9a6a177f5c785516ad4bc40d6ae94f0907
JorgeLG87 <jorge85_6@hotmail.com>
2024-07-23 20:55:22 -0400

    dbConfig modified, for deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,20 +1,22 @@
-//PG-Promise Setup
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER, PG_PASSWORD } =
+  process.env;
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
-//Create the connection
-const cn = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-};
-
-//Open the connection
 const db = pgp(cn);
 
-//If we want more information about the connection
 db.connect()
   .then((cn) => {
     const { user, host, port, database } = cn.client;
@@ -26,6 +28,5 @@ db.connect()
     cn.done();
   })
   .catch((error) => console.log("database connection error", error));
-  
 
 module.exports = db;
